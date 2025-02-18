b2732e0930cdbcd32fdc6a0d8962d6b4d6a40ff0
yuwoii <phish.yuwoi@gmail.com>
2025-02-18 15:55:12 +0100

    vercel niggalink

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [], // Add any external image domains if needed
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
